54be32653928f710fcf4ca0463f8c8733fe5a6d0
abrenzink <abrenzink@bonadmin.com>
2025-11-04 23:01:37 -0300

    feat: adding .gitignore, .env and testing fetch API

--- src/api/affirmations.ts
+++ src/api/affirmations.ts
@@ -1,12 +1,37 @@
-const BASE_URL = "https://affirmation-api.p.rapidapi.com/affirmations";
+const API_URL = import.meta.env.VITE_RAPIDAPI_URL;
+const API_KEY = import.meta.env.VITE_RAPIDAPI_KEY;
+const API_HOST = import.meta.env.VITE_RAPIDAPI_HOST;
 
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': API_KEY,
+		'x-rapidapi-host': API_HOST
+	}
+};
+console.log(options);
 export async function getThemes(): Promise<string[]>{
-    const response = await fetch(BASE_URL + "/themes");
+  try {
+    const response = await fetch(`${API_URL}/themes`, options); 
+    const result = await response.text();
+	  console.log(result);
     return response.json();
+  }
+  catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
 
 export async function getRandomAffirmation(theme: string): Promise<string> {
-  const response = await fetch(BASE_URL + "/random ");
-  const data = await response.json();
-  return data.text || "No affirmation found.";
+  try {
+    const response = await fetch(`${API_URL}/random`, options); 
+    const result = await response.text();
+	  console.log(result);
+    return response.json();
+  }
+  catch (error) {
+    console.error(error);
+    throw error;
+  }
 }
